66fa2d10c19b8ec860bf384a95c04bf41ee03f6a
UHS <ioopl.com@gmail.com>
2021-03-21 13:53:56 +0000

    Added View Controller Classes

---
+++ API-JSON/SecondViewController.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+
+class SecondViewController: UITableViewController {
+    
+    var modelListViewModel = MatchListViewModel()
+    var matchesData = [Match]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // hide empty tableview cells
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        modelListViewModel = MatchListViewModel()
+        modelListViewModel.delegate = self
+        
+        let competition = Competition.defaultCompetitions[0]
+        // ASYNC Call
+        modelListViewModel.fetchUpcomingMatches(competitionId: competition.id)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchesData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        cell.textLabel?.text = matchesData[indexPath.row].homeTeam.name + " vs " + matchesData[indexPath.row].awayTeam.name
+        cell.detailTextLabel?.text = matchesData[indexPath.row].score.winner
+        return cell
+    }
+}
+
+
+extension SecondViewController: MatchListViewModelDelegate {
+    func callbackWhenDataAvailable(matches: [Match]) {
+        matchesData = matches
+        tableView.reloadData()
+    }
+}
