62fd425708647ab5d8b2535e0285fa7f977aee97
UHS <ioopl.com@gmail.com>
2021-03-22 18:13:22 +0000

    Removed comments

--- API-JSON/ViewModel/MatchListViewModel.swift
+++ API-JSON/ViewModel/MatchListViewModel.swift
@@ -13,8 +13,7 @@ class MatchListViewModel {
     
     var service: WebService = DataWebService.shared
     
-    
-        func fetchUpcomingMatches(competitionId: Int) {
+        func asyncFetchUpcomingMatches(competitionId: Int) {
             error = nil
             isLoading = true
     
@@ -35,23 +34,21 @@ class MatchListViewModel {
             }
         }
     
-    func fetchLatestMatches(competitionId: Int) {
+    func asyncFetchLatestMatches(competitionId: Int) {
         error = nil
         isLoading = true
         
         service.fetchLatestMatches(competitionId: competitionId) { (result) in
-            //DispatchQueue.main.async {
                 self.isLoading = false
                 
                 switch result {
                 case .success(let matches):
-                    self.matches = matches.reversed()
+                    self.matches = matches
                     self.delegate?.callbackWhenDataAvailable(matches: matches)
                     
                 case .failure(let error):
                     self.error = error
                 }
-            //}
         }
     }
 }
