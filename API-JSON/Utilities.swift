712f01d062c6c6701bef3040b395e973ded8aa8c
UHS <ioopl.com@gmail.com>
2021-03-22 18:11:39 +0000

    Added Extensions group with specific UI extensions

--- API-JSON/Utilities.swift
+++ API-JSON/Utilities.swift
@@ -27,19 +27,3 @@ struct Utilities {
         }
     }
 }
-
-extension UITableView {
-    func showActivityIndicator() {
-        DispatchQueue.main.async {
-            let activityView = UIActivityIndicatorView(style: .medium)
-            self.backgroundView = activityView
-            activityView.startAnimating()
-        }
-    }
-
-    func hideActivityIndicator() {
-        DispatchQueue.main.async {
-            self.backgroundView = nil
-        }
-    }
-}
